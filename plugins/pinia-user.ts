e2336f3dbf0a45632bfe35a9f17b988d72ee7e9b
Simon <simonrosengren123@gmail.com>
2025-04-24 21:24:44 +0200

    refactor: adjust user store initialization and logging in Supabase plugin

--- plugins/pinia-user.ts
+++ plugins/pinia-user.ts
@@ -1,13 +1,16 @@
 export default defineNuxtPlugin({
   name: 'pinia-user-plugin',
-  enforce: 'pre', // Run before other plugins
+  enforce: 'default', // Run before other plugins
   async setup() {
     const userStore = useUserStore()
     const supabaseUser = useSupabaseUser()
     
     // Initialize user store with current auth state
     watch(supabaseUser, (newUser) => {
-      userStore.setUser(newUser.value)
+      if (newUser) { 
+        console.log(newUser)
+        userStore.setUser(newUser.value)
+      }
     }, { immediate: true })
   }
 })
