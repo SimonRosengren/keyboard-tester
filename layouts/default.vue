c890de6f493ddabb91438b6b36aef5e7d329d980
simonrosengren <simonrosengren>
2026-01-05 22:28:50 +0100

    vercel analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="min-h-screen bg-kq-white dark:bg-kq-black-300 max-sm:px-4
     text-kq-black dark:text-kq-white grid grid-cols-4 sm:grid-cols-8 md:grid-cols-12">
     <NavBar class="col-span-full sm:col-span-6 sm:col-start-2 md:col-start-4" />
+    <Analytics />
     <div class="col-span-full sm:col-span-6 sm:col-start-2 md:col-start-4">
       <slot />
     </div>
@@ -10,3 +11,7 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { Analytics } from '@vercel/analytics/nuxt';
+</script>
